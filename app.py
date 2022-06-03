4bee035a34577ba6fcc242e8c321cadd1db0cb5f
WSokoll <wladek.sokol01@gmail.com>
2022-06-03 16:54:29 +0200

    basic code added

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
