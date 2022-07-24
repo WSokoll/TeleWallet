ce70529c9f3ee14c4336a391c77641396861b397
WSokoll <wladek.sokol01@gmail.com>
2022-07-24 12:56:57 +0200

    add models

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
 import datetime
+
+from sqlalchemy import ForeignKey
+
 from app import db
 
 
@@ -7,3 +10,22 @@ class Roles(db.Model):
     RoleId = db.Column(db.Integer(), primary_key=True)
     RoleName = db.Column(db.String(80), unique=True)
     RoleDescription = db.Column(db.String(255))
+
+
+class Accounts(db.Model):
+    __tablename__ = 'Accounts'
+    AccountId = db.Column(db.Integer(), primary_key=True)
+    AccountActive = db.Column(db.Boolean(), nullable=False)
+    AccountCreatedDate = db.Column(db.DateTime())
+
+
+class Users(db.Model):
+    __tablename__ = 'Users'
+    UserId = db.Column(db.Integer(), primary_key=True)
+    UserName = db.Column(db.String(255))
+    UserSurname = db.Column(db.String(255))
+    UserLogin = db.Column(db.String(255), unique=True, nullable=False)
+    UserPassword = db.Column(db.String(255), nullable=False)
+    UserEmail = db.Column(db.String(255), nullable=False)
+    AccountId = db.Column(db.Integer(), ForeignKey("Accounts.AccountId"), nullable=False, unique=True)
+    RoleId = db.Column(db.Integer(), ForeignKey("Roles.RoleId"), nullable=False)
