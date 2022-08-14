66c6fd445c98728b562a33b3db000d961ad904a2
WSokoll <wladek.sokol01@gmail.com>
2022-08-14 13:56:40 +0200

    validators for internal transactions

---
+++ validators.py
@@ -0,0 +1,50 @@
+from wtforms import ValidationError
+
+from models import User
+
+
+class ValueVSOwnedValidator:
+    """Validates that user has enough money to sent given amount
+
+    :param message: error message
+    """
+
+    def __init__(self, message=None):
+        self.message = message
+
+    def __call__(self, form, field):
+
+        if float(form.value.data) > float(form.owned_value.data):
+            raise ValidationError(
+                self.message
+                or field.gettext(
+                    "Stan Twojego konta jest niższy od wpisanej wartości przelewu."
+
+                )
+            )
+
+        return
+
+
+class UserExistsValidator:
+    """Validates that given name exists in the User table
+
+    :param message: error message
+    """
+
+    def __init__(self, message=None):
+        self.message = message
+
+    def __call__(self, form, field):
+        users = User.query.order_by(User.name).all()
+
+        if field.data not in [user.name for user in users]:
+            raise ValidationError(
+                self.message
+                or field.gettext(
+                    "Osoba, do której chcesz wysłać przelew, nie posiada konta."
+
+                )
+            )
+
+        return
