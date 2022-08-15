6794d8a8bec1a117a9a69ee435f460b0cb7e4102
WSokoll <wladek.sokol01@gmail.com>
2022-08-15 11:12:34 +0200

    account base

--- views/account.py
+++ views/account.py
@@ -1,22 +1,21 @@
-from flask import Blueprint, render_template
-from app import db
+from flask import Blueprint, render_template, abort
+from flask_login import login_required
+
 from models import SubAccount, Currency
 
 bp = Blueprint('bp_account', __name__, template_folder='templates')
 
 
-#login required ....
 # Show account page
 @bp.route('/account/<account_id>/<string:currency_name>', methods=['GET'])
+@login_required
 def get(account_id=0, currency_name='pl'):
+    if account_id == 0:
+        abort(404)
 
-    # 404...
-    # if id == 0:
-    #     return
-
-    # check if sub-account exists
-    currency = Currency.query.get_or_404(name=currency_name)
-    sub_account = SubAccount.query.get_or_404(account_id=account_id, currency_id=currency.id)
+    # check if sub-account exists (jakiś try except od sqlalchemy)
+    currency = Currency.query.filter_by(name=currency_name).first()
+    sub_account = SubAccount.query.filter_by(account_id=account_id, currency_id=currency.id)
 
     return render_template('account.html', currency=currency, sub_account=sub_account)
 
