85eab9eb9b5d58822e069e61e130b7ac8df3741f
WSokoll <wladek.sokol01@gmail.com>
2022-08-15 11:12:23 +0200

    currency exchange p2

--- views/exchange.py
+++ views/exchange.py
@@ -1,21 +1,24 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, flash, redirect, url_for, jsonify
+from flask_login import current_user, login_required
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField
 from wtforms.validators import InputRequired, Regexp
 
-from models import SubAccount, Currency
+from app import db
+from models import SubAccount, Currency, CurrencyExchange
 
 bp = Blueprint('bp_exchange', __name__, template_folder='templates')
 
 
 @bp.route('/exchange/<account_id>', methods=['GET', 'POST'])
-def get(account_id=0):
+@login_required
+def get_post(account_id=0):
 
     if account_id == 0:
         abort(404)
 
     # dynamiczne pole select (jak wybrana jedna waluta, to w drugim do wyboru inne, poza tą wybraną)
-    # walidator dostępnej kwoty...
+    # walidator dostępnej kwoty...(jakoś we froncie? z wykożystaniem sub_accounts i currencies)
     # dynamicznie zmieniające sie pole value_to w zależności od wpisanej kwoty value_from
 
     sub_accounts = SubAccount.query.filter_by(account_id=account_id).all()
@@ -28,7 +31,7 @@ def get(account_id=0):
     class ExchangeForm(FlaskForm):
         currency_from = SelectField('Z', validators=[InputRequired()], choices=currency_names)
         currency_to = SelectField('Na', validators=[InputRequired()], choices=currency_names)
-        value_from = StringField('Wartość przed', validators=[InputRequired()])
+        value_from = StringField('Wartość przed', validators=[InputRequired(), Regexp(r'^[0-9.]*$')])
         value_to = StringField('Wartość po', render_kw={'readonly': True}, validators=[Regexp(r'^[0-9.]*$')])
 
     form = ExchangeForm()
@@ -36,12 +39,47 @@ def get(account_id=0):
     form.value_to.data = '0.0'
 
     if form.validate_on_submit():
-        pass
+        currency_from = Currency.query.filter_by(name=form.currency_from.data).first()
+        currency_to = Currency.query.filter_by(name=form.currency_to.data).first()
+
+        sub_account_from = SubAccount.query.filter_by(account_id=account_id, currency_id=currency_from.id)
+        sub_account_to = SubAccount.query.filter_by(account_id=account_id, currency_id=currency_to.id)
+
+        value_to = float(form.value_from.data) * currency_from.exchange_rate * currency_to.exchange_rate
+
+        # will it work (current_user.id)????
+        exchange = CurrencyExchange(user_id=current_user.id,
+                                    currency_from=currency_from.id,
+                                    currency_to=currency_to.id,
+                                    value_old=float(form.value_from.data),
+                                    value_new=value_to)
+
+        with db.session.begin():
+            sub_account_from.balance = sub_account_from.balance - float(form.value_from.data)
+            sub_account_to.balance = sub_account_to.balance + value_to
+
+            db.session.add(exchange)
+
+        flash('Wymiana zakończona sukcesem.')
+        return redirect(url_for('bp_account.get', account_id=account_id, currency_name='pl'))
+
     elif form.is_submitted():
-        pass
+        for field_name, errors in form.errors.items():
+            for err in errors:
+                flash(f"{form._fields[field_name].label.text}: {err}", 'error')
 
-    return render_template('currency_exchange.html', form=form)
+    return render_template('currency_exchange.html', form=form, sub_accounts=sub_accounts, currencies=currencies)
 
 
-def rest_currencies():
-    pass
+@bp.route('/exchange/<account_id>/<currency_name>/other')
+@login_required
+def rest_currencies(account_id=0, currency_name=''):
+    if account_id == 0 or currency_name == '':
+        abort(404)
+
+    sub_accounts = SubAccount.query.filter_by(account_id=account_id).all()
+    currencies = []
+    for sa in sub_accounts:
+        currencies.append(Currency.query.filter_by(id=sa.currency_id).first())
+
+    return jsonify({'other': [c.name for c in currencies if c.name != currency_name]})
