4663e9c3931f56309f6810180d525e6828bfcd8a
WSokoll <wladek.sokol01@gmail.com>
2022-08-18 20:26:06 +0200

    Bug fixes and internal transaction page

--- views/account.py
+++ views/account.py
@@ -7,7 +7,7 @@ bp = Blueprint('bp_account', __name__, template_folder='templates')
 
 
 # Show account page
-@bp.route('/account/<account_id>/<string:currency_name>', methods=['GET'], defaults={'currency_name': 'zł'})
+@bp.route('/account/<int:account_id>/<string:currency_name>', methods=['GET'], defaults={'currency_name': 'zł'})
 @login_required
 def get(account_id=0, currency_name='zł'):
     if account_id == 0:
@@ -15,7 +15,7 @@ def get(account_id=0, currency_name='zł'):
 
     # check if sub-account exists (jakiś try except od sqlalchemy) tak samo z walutą
     currency = Currency.query.filter_by(name=currency_name).first()
-    sub_account = SubAccount.query.filter_by(account_id=account_id, currency_id=currency.id)
+    sub_account = SubAccount.query.filter_by(account_id=account_id, currency_id=currency.id).first()
 
     return render_template('account.html', currency=currency, sub_account=sub_account)
 
